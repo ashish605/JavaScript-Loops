524e52302e3bf2da3787f7f46f4d2391a34f7ddd
ashish605 <ashishvs605@gmail.com>
2024-09-07 15:49:03 +0530

    Initial commit

---
+++ scprit.js
@@ -0,0 +1,58 @@
+console.log("hello")
+
+// let a=1;
+// while(a<10)
+//     {
+//         console.log(a+" ")
+//         a++
+//     }
+
+
+// for( let i=1;i<=10;i++)
+//     {
+//         console.log(i)
+
+//     }
+
+
+// let obj = {
+//     name: "ashish",
+//     class: "Btech cse",
+//     company: "google"
+// }
+
+// for (const key in obj) {
+//     console.log(element)
+// }
+
+// const a=[34,67,32,12]
+
+// for (const c of a) {
+//     console.log(c)
+    
+// }
+
+// function ashish(name)
+// {
+//      console.log("hey "+name+" how are you")
+// }
+
+// ashish("veadehi")
+
+
+// function sum(a,b)
+// {
+//     return a+b
+// }
+
+// result=sum(1,2)
+
+// console.log("the sum is ",result)
+
+
+const func1 =(x)=>{
+    console.log("i am a arrow funaction",x)
+}
+
+func1(456)
+func1(90)
